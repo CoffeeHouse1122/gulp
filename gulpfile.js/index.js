3f6cac2d4caf5deaac9f1598ba23c23fe725976b
CoffeeHouse1122 <1158165115@qq.com>
2020-11-19 11:40:32 +0800

    gulp webpack配合完成es6 -> es5

--- gulpfile.js/index.js
+++ gulpfile.js/index.js
@@ -1,30 +1,57 @@
 const { src, dest, series, parallel } = require("gulp");
-const babel = require("gulp-babel");
 const uglify = require("gulp-uglify");
 const obfuscate = require("gulp-obfuscate");
 const rename = require("gulp-rename");
 const minifyCss = require("gulp-clean-css");
 const autoprefixer = require("gulp-autoprefixer");
+const webpack = require("webpack-stream");
+const named = require("vinyl-named");
 
 function javascript() {
-  return src("src/*.js")
-    // .pipe(
-    //   babel({
-    //     presets: ["@babel/env"],
-    //   })
-    // )
-    .pipe(uglify({ mangle: { toplevel: true } })) // 压缩并字母化变量
-    // .pipe(obfuscate({ replaceMethod: obfuscate.LOOK_OF_DISAPPROVAL })) // 混淆代码 默认参数LOOK_OF_DISAPPROVAL - ಠ_ಠ777; ZALGO - H͇̬͔̳̖̅̒ͥͧẸ̖͇͈͍̱̭̌͂͆͊_C͈OM̱̈́͛̈ͩ͐͊ͦEͨ̓̐S̬̘͍͕͔͊̆̑̈́̅
-    .pipe(
-      rename({
-        dirname: "js", // 文件路径
-        // basename: "text", // 文件名
-        prefix: "", // 文件名前缀
-        suffix: ".min", // 文件名后缀
-        extname: ".js", // 文件扩展名
-      })
-    )
-    .pipe(dest("output/"));
+  return (
+    src("src/*.js")
+      .pipe(named())
+      .pipe(
+        webpack({
+          mode: "none",
+          module: {
+            rules: [
+              {
+                test: /\.js$/,
+                exclude: /(node_modules|bower_components)/,
+                use: {
+                  loader: "babel-loader",
+                  options: {
+                    presets: [
+                      [
+                        "@babel/preset-env",
+                        {
+                          useBuiltIns: "usage", // "entry" = @babel/polyfill
+                          corejs: { version: 3, proposals: true },
+                        },
+                      ],
+                    ],
+                    plugins: ["@babel/plugin-transform-runtime"],
+                  },
+                },
+              },
+            ]
+          }
+        })
+      )
+      .pipe(uglify({ mangle: { toplevel: true } })) // 压缩并字母化变量
+      // .pipe(obfuscate({ replaceMethod: obfuscate.LOOK_OF_DISAPPROVAL })) // 混淆代码 默认参数LOOK_OF_DISAPPROVAL - ಠ_ಠ777; ZALGO - H͇̬͔̳̖̅̒ͥͧẸ̖͇͈͍̱̭̌͂͆͊_C͈OM̱̈́͛̈ͩ͐͊ͦEͨ̓̐S̬̘͍͕͔͊̆̑̈́̅
+      .pipe(
+        rename({
+          dirname: "js", // 文件路径
+          // basename: "text", // 文件名
+          prefix: "", // 文件名前缀
+          suffix: ".min", // 文件名后缀
+          extname: ".js", // 文件扩展名
+        })
+      )
+      .pipe(dest("output/"))
+  );
 }
 
 function css() {
@@ -44,7 +71,7 @@ function css() {
     .pipe(
       minifyCss({
         specialComments: "all", //保留所有特殊前缀
-        format: 'keep-breaks', // 增加可读性
+        format: "keep-breaks", // 增加可读性
       })
     )
     .pipe(
---
+++ src/main.js
@@ -0,0 +1,63 @@
+// axios
+const $axios = axios.create({
+  method: "post",
+  headers: {
+    "Content-Type": "application/x-www-form-urlencoded",
+  },
+  timeout: 30000,
+});
+
+// request拦截器
+$axios.interceptors.request.use(
+  (config) => {
+
+    if (
+      config.method === "post" &&
+      config.headers["Content-Type"] !== "multipart/form-data"
+    ) {
+      config.data = Qs.stringify(config.data);
+    }
+    return config;
+  },
+  (error) => {
+    Promise.reject(error);
+  }
+);
+
+// respone拦截器
+$axios.interceptors.response.use(
+  (response) => {
+    const res = response.data;
+    return res;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
+
+let axiosFunc = {
+  getPreCount: async function () {
+    let res = await $axios.post("/yuyue/count");
+    if (res.code == 0) {
+      let preCount = Math.floor(res.data.num / 10000);
+      if(preCount >= 50) {
+        $(".reached, .progress").addClass("cur")
+      }
+    }
+  },
+}
+
+$(function () {
+    // 首屏视频
+    var isAndroid = navigator.userAgent.indexOf("Android") > -1 || navigator.userAgent.indexOf("Linux") > -1;
+    var videoHTML;
+    if (isAndroid) {
+      videoHTML = ""
+    } else {
+      videoHTML = `<video src="./media/kv.mp4" id="videoKv" x5-video-player-type="h5" preload="metadata" playsinline="true" webkit-playsinline="true" x-webkit-airplay="true" x5-video-orientation="portraint" autoplay="" loop="" muted="muted" poster=""></video>`;
+    }
+    $(".videoBox").html(videoHTML);
+
+    axiosFunc.getPreCount()
+
+})
